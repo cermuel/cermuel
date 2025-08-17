3be97fd2a710a40f96da90e4687462a421d45a18
Ngene Samuel Obasi <81524684+cermuel@users.noreply.github.com>
2025-08-17 18:31:10 +0100

    Update Hours worked

--- src/App.tsx
+++ src/App.tsx
@@ -65,7 +65,7 @@ function App() {
             </div>
             <div className="flex flex-col items-center md:items-end">
               <h1 className="font-bold text-2xl md:text-5xl text-[#d0d3f5fa]">
-                1k<span className="md:text-2xl text-base">+</span>
+                4k<span className="md:text-2xl text-base">+</span>
               </h1>
               <p className="max-md:text-sm text-gray-500">Hours worked</p>
             </div>
